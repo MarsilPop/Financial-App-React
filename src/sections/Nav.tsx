edd5f51003e276e3d5474c53b5ed4ff0b78149e6
Marcell <149784148+MarsilPop@users.noreply.github.com>
2024-06-12 10:32:47 +0200

    refactored the Nav

--- src/sections/Nav.tsx
+++ src/sections/Nav.tsx
@@ -1,45 +1,45 @@
 import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
 import Kobodrop from "../assets/logos/kobodrop_logo.svg";
+import ButtonLink from "../common/ButtonLink";
 
-const Nav = () => {
+function Nav() {
+  const textLinkClasses =
+    "text-gray-500 hover:text-gray-900 active:text-gray-400";
+
+  const navLinks = [
+    { href: "#features", children: "Features" },
+    { href: "#partners", children: "Partners" },
+    { href: "#reviews", children: "Reviews" },
+  ];
+
   return (
     <Disclosure as="nav">
       {({ open }) => (
         <>
-          <div className="flex h-[15vh] max-w-7xl items-center justify-between px-8">
+          <div className="flex h-[15vh] max-w-7xl items-center justify-between px-8 lg:px-12 xl:m-auto">
             <div className="flex">
-              <a href="#">
-                <img src={Kobodrop} alt="Kobodrop logo" />
-              </a>
-              <div className="ml-4 hidden items-center space-x-6 sm:flex">
-                <a
-                  className="text-gray-500 hover:text-gray-900 active:text-gray-400"
-                  href="#features"
-                >
-                  Features
-                </a>
-                <a
-                  className="text-gray-500 hover:text-gray-900 active:text-gray-400"
-                  href="#partners"
-                >
-                  Partners
-                </a>
-                <a
-                  className="text-gray-500 hover:text-gray-900 active:text-gray-400"
-                  href="#reviews"
-                >
-                  Reviews
-                </a>
+              <ButtonLink
+                href="#"
+                children={<img src={Kobodrop} alt="Kobodrop logo" />}
+              />
+              <div className="ml-4 hidden items-center space-x-6 sm:ml-6 sm:flex lg:ml-8 lg:space-x-8">
+                {navLinks.map((link, index) => (
+                  <ButtonLink
+                    key={index}
+                    href={link.href}
+                    children={link.children}
+                    className={textLinkClasses}
+                  />
+                ))}
               </div>
             </div>
-            <a
+            <ButtonLink
               href="https://play.google.com"
               target="_blank"
+              children="Install Kobodrop"
               className="hidden rounded-xl bg-gray-800 px-5 py-3 text-white hover:bg-gray-900 active:bg-gray-600 sm:flex"
-            >
-              Install Kobodrop
-            </a>
+            />
             <Disclosure.Button className="rounded-md p-2 text-gray-500 hover:bg-gray-700 hover:text-white sm:hidden">
               {open ? (
                 <XMarkIcon className="block h-6 w-6" />
@@ -48,21 +48,22 @@ const Nav = () => {
               )}
             </Disclosure.Button>
           </div>
-          <Disclosure.Panel>
-            <Disclosure.Button className="block" as="a" href="#features">
-              Features
-            </Disclosure.Button>
-            <Disclosure.Button className="block" as="a" href="#partners">
-              Partners
-            </Disclosure.Button>
-            <Disclosure.Button className="block" as="a" href="#reviews">
-              Reviews
-            </Disclosure.Button>
+          <Disclosure.Panel className="space-y-1 px-4 sm:hidden">
+            {navLinks.map((link, index) => (
+              <Disclosure.Button
+                className="block"
+                as="a"
+                href="#features"
+                key={index}
+              >
+                {link.children}
+              </Disclosure.Button>
+            ))}
           </Disclosure.Panel>
         </>
       )}
     </Disclosure>
   );
-};
+}
 
 export default Nav;
