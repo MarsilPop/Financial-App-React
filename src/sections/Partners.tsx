6812de1720c0eb40efcb713cc004404d467b6ec0
Marcell <149784148+MarsilPop@users.noreply.github.com>
2024-06-13 14:04:20 +0200

    LogoWall component and Partners section done

--- src/sections/Partners.tsx
+++ src/sections/Partners.tsx
@@ -1,6 +1,16 @@
 import Klarna from "../assets/logo-wall/klarna.png";
 import Stripe from "../assets/logo-wall/stripe.png";
 import PayPal from "../assets/logo-wall/paypal.png";
+import LogoWall from "../common/LogoWall";
+import Bedc from "../assets/logo-wall/bedc.png";
+import Mastercard from "../assets/logo-wall/mastercard.png";
+import Mtn from "../assets/logo-wall/mtn.png";
+import Airtel from "../assets/logo-wall/airtel.png";
+import Payoneer from "../assets/logo-wall/payoneer.png";
+import Visa from "../assets/logo-wall/visa.png";
+import Mobile from "../assets/logo-wall/mobile.png";
+import Ikeja from "../assets/logo-wall/ikeja.png";
+import Dstv from "../assets/logo-wall/dstv.png";
 
 function Partners() {
   return (
@@ -16,11 +26,40 @@ function Partners() {
         </p>
       </article>
       <div className="xl:flex xl:justify-center">
-        <article className="mb-8 flex justify-center gap-8 text-center">
-          <img src={Klarna} alt="Klarna logo" className="w-20 md:w-36" />
-          <img src={Stripe} alt="Strip logo" className="w-20 md:w-36" />
-          <img src={PayPal} alt="Paypal logo" className="w-20 md:w-36" />
-        </article>
+        <LogoWall
+          src1={Klarna}
+          src2={Stripe}
+          src3={PayPal}
+          alt1="Klarna logo"
+          alt2="Stripe logo"
+          alt3="PayPal logo"
+        />
+        <LogoWall
+          src1={Bedc}
+          src2={Mastercard}
+          src3={Mtn}
+          alt1="Bedc logo"
+          alt2="Mastercard logo"
+          alt3="Mtn logo"
+        />
+      </div>
+      <div className="xl:flex xl:justify-center">
+        <LogoWall
+          src1={Airtel}
+          src2={Payoneer}
+          src3={Visa}
+          alt1="Airtel logo"
+          alt2="Payoneer logo"
+          alt3="Visa logo"
+        />
+        <LogoWall
+          src1={Mobile}
+          src2={Ikeja}
+          src3={Dstv}
+          alt1="Mobile logo"
+          alt2="Ikeja logo"
+          alt3="Dstv logo"
+        />
       </div>
     </section>
   );
